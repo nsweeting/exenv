8c3ea6fcffba479cca238b310b05672b70761338
Nick Sweeting <nsweeting@gmail.com>
2021-02-02 14:30:53 -0500

    Fix elixir warnings

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Exenv.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :crypto],
       mod: {Exenv, []}
     ]
   end
